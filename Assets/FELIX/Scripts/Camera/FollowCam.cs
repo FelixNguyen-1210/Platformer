23a716333389d55f4f11b39b455af0496dd2acd6
FelixNguyen-1210 <felix.patn@gmail.com>
2026-03-22 23:42:44 -0500

    Setup Project

--- Assets/FELIX/Scripts/Camera/FollowCam.cs
+++ Assets/FELIX/Scripts/Camera/FollowCam.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
-    public Direction dir;
+    // public Direction dir;
     private Camera m_cam;
     private Vector3 m_startingPos;
     private Vector2 m_camSize;
@@ -18,45 +18,45 @@ public class FollowCam : MonoBehaviour
 
     private void Update()
     {
-        switch(dir)
-        {
-            case Direction.Up:
-                if (m_cam.transform.position.y < m_startingPos.y)
-                {
-                    transform.position = new Vector3(
-            m_cam.transform.position.x,
-            m_startingPos.y,
-            transform.position.z);
-                }else
-                {
-                    transform.position = new Vector3(
-                        m_cam.transform.position.x,
-                        m_cam.transform.position.y, 0f
-                        ) ;
-                }
-                break;
-            case Direction.Down:
-                if (m_cam.transform.position.y > m_startingPos.y)
-                {
-                    transform.position = new Vector3(
-            m_cam.transform.position.x,
-            m_startingPos.y,
-            transform.position.z);
-                }
-                else
-                {
-                    transform.position = new Vector3(
-                        m_cam.transform.position.x,
-                        m_cam.transform.position.y, 0f
-                        );
-                }
-                break;
-            default:
-                transform.position = new Vector3(
-                        m_cam.transform.position.x,
-                        m_cam.transform.position.y, 0f
-                        );
-                break;
-        }    
+        // switch(dir)
+        // {
+        //     case Direction.Up:
+        //         if (m_cam.transform.position.y < m_startingPos.y)
+        //         {
+        //             transform.position = new Vector3(
+        //     m_cam.transform.position.x,
+        //     m_startingPos.y,
+        //     transform.position.z);
+        //         }else
+        //         {
+        //             transform.position = new Vector3(
+        //                 m_cam.transform.position.x,
+        //                 m_cam.transform.position.y, 0f
+        //                 ) ;
+        //         }
+        //         break;
+        //     case Direction.Down:
+        //         if (m_cam.transform.position.y > m_startingPos.y)
+        //         {
+        //             transform.position = new Vector3(
+        //     m_cam.transform.position.x,
+        //     m_startingPos.y,
+        //     transform.position.z);
+        //         }
+        //         else
+        //         {
+        //             transform.position = new Vector3(
+        //                 m_cam.transform.position.x,
+        //                 m_cam.transform.position.y, 0f
+        //                 );
+        //         }
+        //         break;
+        //     default:
+        //         transform.position = new Vector3(
+        //                 m_cam.transform.position.x,
+        //                 m_cam.transform.position.y, 0f
+        //                 );
+        //         break;
+        // }    
     }
 }
